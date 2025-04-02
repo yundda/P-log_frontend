a5fd4c7c4e88209d9f6702efec0b66a0b6d1ea18
suvd0377 <suvd037@icloud.com>
2025-04-02 15:06:31 +0900

    [design]: 250402 로그인 ui 구현 (진행중)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Test from './components/test';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 import './style/index.css';
 function App() {
   return (
     <>
-      <Test />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
--- src/components/test.jsx
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-  return (
-    <>
-      <div className="w-10 h-4 text-3xl underline text-red-400">hi?/??</div>
-    </>
-  );
-}
